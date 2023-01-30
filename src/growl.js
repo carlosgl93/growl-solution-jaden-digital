e9034180778a9cdd0da5c071c6cd95fbae1c72fc
Carlos <cgumuciolabbe@gmail.com>
2023-01-30 19:35:00 +1000

    hide growl  on timeout feature implemented in the useGrowl hook

--- src/growl.js
+++ src/growl.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import './growl.css'
+import "./growl.css";
 
 export const Growl = ({ active, message, onDismissed }) => (
   <div className={`growl${active ? " active" : ""}`}>
     {message}
     <div onClick={onDismissed} className="growl-close" />
   </div>
-)
+);
 
-export function useGrowl() {
-    // state of the growl
-    const [growlActive, setGrowlActive] = React.useState(false)
+export function useGrowl(timeout = 3000) {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false);
 
-    return [
-        // the first arg is the state
-        growlActive, 
+  React.useEffect(() => {
+    if (growlActive && timeout) {
+      let hideGrowl = setTimeout(() => setGrowlActive(false), timeout);
 
-        // the second arg is a fn that allows you to safely set its state
-        (active) => {
-            setGrowlActive(active)
-        },
-    ]
+      return () => {
+        clearTimeout(hideGrowl);
+      };
+    } else return;
+  }, [growlActive]);
+
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active);
+    },
+  ];
 }
